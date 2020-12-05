2d6af50f745dbe13b03fa404fb6f5953c44e72e0
vfdxvffd <2201986113@qq.com>
2020-12-05 20:32:01 +0800

    重新编写redis的实现类，去掉RedisUtil的使用

--- src/main/java/com/vfd/demo/service/impl/RedisServiceImpl.java
+++ src/main/java/com/vfd/demo/service/impl/RedisServiceImpl.java
@@ -1,9 +1,66 @@
-package com.vfd.demo.service.impl;/**
-* @PackageName: com.vfd.demo.service.impl
-* @ClassName: RedisServiceImpl
-* @Description:
-* @author: vfdxvffd
-* @date: 12/5/20 7:33 PM
-*/
-public class RedisServiceImpl {
+package com.vfd.demo.service.impl;
+
+import com.vfd.demo.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @PackageName: com.vfd.demo.service.impl
+ * @ClassName: RedisServiceImpl
+ * @Description:
+ * @author: vfdxvffd
+ * @date: 12/5/20 7:33 PM
+ */
+@Service
+public class RedisServiceImpl implements RedisService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public boolean set(String key, Object value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean set(String key, Object value, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+            } else {
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public Object get(String key) {
+        return key == null ? null : redisTemplate.opsForValue().get(key);
+    }
+
+    @Override
+    public void del(String... key) {
+        if (key != null && key.length > 0) {
+            if (key.length == 1) {
+                redisTemplate.delete(key[0]);
+            } else {
+                redisTemplate.delete((Collection<String>) CollectionUtils.arrayToList(key));
+            }
+        }
+    }
 }
