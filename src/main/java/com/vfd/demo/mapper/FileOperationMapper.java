e9806601f28cce2691744b35851e0512d0095898
vfdxvffd <2201986113@qq.com>
2021-01-22 00:09:24 +0800

    处理上传上传文件时的数据库操作

--- src/main/java/com/vfd/demo/mapper/FileOperationMapper.java
+++ src/main/java/com/vfd/demo/mapper/FileOperationMapper.java
@@ -1,9 +1,40 @@
-package com.vfd.demo.mapper;/**
+package com.vfd.demo.mapper;
+
+import com.vfd.demo.bean.FileInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
  * @PackageName: com.vfd.demo.mapper
  * @ClassName: FileOperationMapper
  * @Description:
  * @author: vfdxvffd
  * @date: 2021/1/21 下午8:03
  */
+@Mapper
+@Repository
 public interface FileOperationMapper {
+
+    /**
+     * 上传一个文件，将信息保存到数据库
+     * @param fileInfo
+     * @return
+     */
+    Boolean saveFile (FileInfo fileInfo);
+
+    /**
+     * 通过父目录id获取其下所有文件
+     * @param fid
+     * @return
+     */
+    List<FileInfo> getFilesByFid(Integer fid);
+
+    /**
+     * 通过id获得location
+     * @param id
+     * @return
+     */
+    String getLocationById(Integer id);
 }
