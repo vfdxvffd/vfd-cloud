ed8af71b75ac03b481b8bab271a4a4884a3fb0e8
vfdxvffd <2201986113@qq.com>
2021-01-22 00:09:59 +0800

    处理上传上传文件时的数据库操作

--- src/main/java/com/vfd/demo/service/impl/FileOperationServiceImpl.java
+++ src/main/java/com/vfd/demo/service/impl/FileOperationServiceImpl.java
@@ -1,9 +1,38 @@
-package com.vfd.demo.service.impl;/**
+package com.vfd.demo.service.impl;
+
+import com.vfd.demo.bean.FileInfo;
+import com.vfd.demo.mapper.FileOperationMapper;
+import com.vfd.demo.service.FileOperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
  * @PackageName: com.vfd.demo.service.impl
  * @ClassName: FileOperationServiceImpl
  * @Description:
  * @author: vfdxvffd
  * @date: 2021/1/21 下午8:49
  */
-public class FileOperationServiceImpl {
+@Service
+public class FileOperationServiceImpl implements FileOperationService {
+
+    @Autowired
+    FileOperationMapper fileOperationMapper;
+
+    @Override
+    public Boolean saveFile(FileInfo fileInfo) {
+        return fileOperationMapper.saveFile(fileInfo);
+    }
+
+    @Override
+    public List<FileInfo> getFilesByFid(Integer fid) {
+        return fileOperationMapper.getFilesByFid(fid);
+    }
+
+    @Override
+    public String getLocationById(Integer id) {
+        return fileOperationMapper.getLocationById(id);
+    }
 }
