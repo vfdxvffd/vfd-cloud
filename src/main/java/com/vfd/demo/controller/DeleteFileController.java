ee8d7f9fe75f22a2b882fa75145deaeb6ae53c86
vfdxvffd <2201986113@qq.com>
2021-01-23 21:32:38 +0800

    删除文件（夹）的controller

--- src/main/java/com/vfd/demo/controller/DeleteFileController.java
+++ src/main/java/com/vfd/demo/controller/DeleteFileController.java
@@ -1,9 +1,33 @@
-package com.vfd.demo.controller;/**
+package com.vfd.demo.controller;
+
+import com.vfd.demo.service.FileOperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
  * @PackageName: com.vfd.demo.controller
  * @ClassName: DeleteFileController
  * @Description:
  * @author: vfdxvffd
  * @date: 2021/1/23 下午8:42
  */
+@Controller
 public class DeleteFileController {
+
+    @Autowired
+    FileOperationService fileOperationService;
+
+    @ResponseBody
+    @RequestMapping("delete")
+    public String deleteFile (@RequestParam("id") Integer id) {
+        fileOperationService.deleteFileOnDiskById(id);
+        if (fileOperationService.deleteFileById(id)) {
+            return "success";
+        } else {
+            return "fail";
+        }
+    }
 }
