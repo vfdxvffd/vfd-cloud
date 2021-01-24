e0b1bf6b47fb36c6077b7386b5b5be17d2a6360b
vfdxvffd <2201986113@qq.com>
2021-01-24 16:52:20 +0800

    增加转化文件大小信息的方法

--- src/main/java/com/vfd/demo/bean/FileInfo.java
+++ src/main/java/com/vfd/demo/bean/FileInfo.java
@@ -1,5 +1,8 @@
 package com.vfd.demo.bean;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 /**
  * @PackageName: com.vfd.demo.bean
  * @ClassName: FileInfo
@@ -108,5 +111,39 @@ public class FileInfo {
 
     public void setType(Integer type) {
         this.type = type;
+    }
+
+    public String convertToSize() {
+        BigDecimal fileSize = new BigDecimal(this.len);
+        BigDecimal param = new BigDecimal(1024);
+        int count = 0;
+        while(fileSize.compareTo(param) > 0 && count < 5)
+        {
+            fileSize = fileSize.divide(param);
+            count++;
+        }
+        DecimalFormat df = new DecimalFormat("#.##");
+        String result = df.format(fileSize) + "";
+        switch (count) {
+            case 0:
+                result += "B";
+                break;
+            case 1:
+                result += "KB";
+                break;
+            case 2:
+                result += "MB";
+                break;
+            case 3:
+                result += "GB";
+                break;
+            case 4:
+                result += "TB";
+                break;
+            case 5:
+                result += "PB";
+                break;
+        }
+        return result;
     }
 }
