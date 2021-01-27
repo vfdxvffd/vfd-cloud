273e9d68d8a96b5a4114e99106fbab0b319458b9
vfdxvffd <2201986113@qq.com>
2021-01-27 15:09:43 +0800

    增加拦截器

--- src/main/java/com/vfd/demo/interceptor/LoginInterceptor.java
+++ src/main/java/com/vfd/demo/interceptor/LoginInterceptor.java
@@ -1,9 +1,28 @@
-package com.vfd.demo.interceptor;/**
+package com.vfd.demo.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
  * @PackageName: com.vfd.demo.interceptor
  * @ClassName: LoginInterceptor
  * @Description:
  * @author: vfdxvffd
  * @date: 2021/1/27 下午12:53
  */
-public class LoginInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object userId = request.getSession().getAttribute("loginUserId");
+        if (userId == null) {
+            request.setAttribute("err","无权限访问，请先登陆");
+            request.getRequestDispatcher("/").forward(request,response);
+            //response.sendRedirect("/");
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
