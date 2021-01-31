3a9e7f7b678a22e3c10aed4b94f58e9700933b18
vfdxvffd <2201986113@qq.com>
2021-01-31 21:18:58 +0800

    带上用户id的上传

--- src/main/java/com/vfd/demo/controller/UploadFileController.java
+++ src/main/java/com/vfd/demo/controller/UploadFileController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Timestamp;
@@ -44,16 +45,15 @@ public class UploadFileController {
     @ResponseBody
     @PostMapping("/uploadFile")
     public List<FileInfo> uploadFile(@RequestPart("select_file")MultipartFile[] files,
-                                     @RequestParam("username") String username,
+                                     @RequestParam("id") Integer id,
                                      @RequestParam("fid") Integer fid,
                                      @RequestParam("location") String location) {
-//        ModelAndView modelAndView = new ModelAndView("forward:/enterFile");
         List<FileInfo> result = new ArrayList<>();
         if (files.length > 0) {
             try {
                 for (MultipartFile file:files) {
                     if (!file.isEmpty()) {
-                        FileInfo fileInfo = new FileInfo(null,file.getOriginalFilename(), file.getSize(), fid, location,1,new Timestamp(new Date().getTime()));
+                        FileInfo fileInfo = new FileInfo(null,file.getOriginalFilename(), file.getSize(), fid, location,1,new Timestamp(new Date().getTime()),id);
                         Boolean saveFile = fileOperationService.saveFile(fileInfo);
                         if (!saveFile) {        //往数据库添加失败
                             fileInfo.setType(0);//如果往数据库添加失败就将这条记录的type改为0,返回前端提示出来
@@ -71,30 +71,16 @@ public class UploadFileController {
             }
         }
         return result;
-//        modelAndView.addObject("username",username);
-//        modelAndView.addObject("fid",fid); //文件id
-//        //modelAndView.addObject("location",location);  //位置
-//        return modelAndView;
     }
 
-
-    /**
-     modelAndView.addObject("username",exampleInputEmail);
-     modelAndView.addObject("id",login);     //将用户id发送到index页面
-     modelAndView.addObject("fid",-1*login); //文件id
-     modelAndView.addObject("location","allFiles");  //位置
-     modelAndView.addObject("files",new File("/
-     */
     //通过文件夹的id进入某个文件夹fid
     @ResponseBody
     @PostMapping("/enterFile")
-    public Map<String, Object> enterFile(@RequestParam("username") String username,
+    public Map<String, Object> enterFile(@RequestParam("id") Integer id,
                                   @RequestParam("fid") Integer fid) {
         Map<String,Object> result = new HashMap<>();
-//        ModelAndView modelAndView = new ModelAndView("index");
-        FileInfo fileInfo = fileOperationService.getFileById(fid);  //父目录对象
-//        modelAndView.addObject("username",username);
-        List<FileInfo> all = fileOperationService.getFilesByFid(fid);
+        FileInfo fileInfo = fileOperationService.getFileById(fid, id);  //父目录对象
+        List<FileInfo> all = fileOperationService.getFilesByFid(fid, id);
         List<FileInfo> dirs = new ArrayList<>();     //文件夹
         List<FileInfo> docs = new ArrayList<>();     //文档
         for (FileInfo f : all) {
@@ -104,8 +90,6 @@ public class UploadFileController {
                 docs.add(new FileInfo(f));
             }
         }
-//        modelAndView.addObject("dirs",dirs);
-//        modelAndView.addObject("docs",docs);
         result.put("dirs",dirs);
         result.put("docs",docs);
         String[] dirId = fileInfo.getLocation().split(">");
@@ -113,27 +97,23 @@ public class UploadFileController {
         for (String dir:dirId) {
             String[] split = dir.split("\\.");
             if (split.length == 2)
-                fileInfos.add(new FileInfo(Integer.parseInt(split[0]),split[1]));
+                fileInfos.add(new FileInfo(Integer.parseInt(split[0]), split[1], id));
         }
-//        fileInfos.add(fileInfo);
-//        modelAndView.addObject("currentDir",fileInfo);
-//        modelAndView.addObject("path",fileInfos);
         result.put("currentDir",fileInfo);
         result.put("path",fileInfos);
-//        return modelAndView;
         return result;
     }
 
 
     @ResponseBody
     @PostMapping("/mkdir")
-    public FileInfo mkdir(@RequestParam("username") String username,
+    public FileInfo mkdir(@RequestParam("id") Integer id,
                               @RequestParam("fid") Integer fid,
                               @RequestParam("location") String location,
                               @RequestParam("inputDir") String dirName,
-                              @RequestParam("f_name") String fName) {
-//        ModelAndView modelAndView = new ModelAndView("forward:/enterFile");
-        FileInfo result = new FileInfo(dirName, 0L, fid, location + ">" + fid + "." + fName, 0, new Timestamp(new Date().getTime()));
+                              @RequestParam("f_name") String fName,
+                              HttpSession session) {
+        FileInfo result = new FileInfo(dirName, 0L, fid, location + ">" + fid + "." + fName, 0, new Timestamp(new Date().getTime()), id);
         Boolean saveFile = fileOperationService.saveFile(result);
         if (saveFile) {
             return result;
@@ -142,8 +122,5 @@ public class UploadFileController {
             fail.setId(0);
             return fail;
         }
-//        modelAndView.addObject("username",username);
-//        modelAndView.addObject("fid",fid);
-//        return modelAndView;
     }
 }
