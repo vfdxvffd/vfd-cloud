e9f27a8c8ab9f1afbb8dbf319e62dd9976ca8c45
vfdxvffd <2201986113@qq.com>
2021-02-04 02:45:13 +0800

    加密模块的单元测试类

--- src/test/java/com/vfd/demo/Sm4Test.java
+++ src/test/java/com/vfd/demo/Sm4Test.java
@@ -1,9 +1,26 @@
-package com.vfd.demo;/**
+package com.vfd.demo;
+
+import com.vfd.demo.controller.UploadFileController;
+import com.vfd.demo.service.FileOperationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
  * @PackageName: com.vfd.demo
  * @ClassName: Sm4Test
  * @Description:
  * @author: vfdxvffd
  * @date: 2021/2/2 下午8:57
  */
+@SpringBootTest
 public class Sm4Test {
+
+    @Autowired
+    FileOperationService fileOperationService;
+
+    @Test
+    void decrypt () {
+        fileOperationService.decryptFile(UploadFileController.PROJECT_DIR+"29/~tmp", 29);
+    }
 }
