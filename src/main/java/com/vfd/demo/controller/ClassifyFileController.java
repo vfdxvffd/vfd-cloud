e6ea46d38a54ae0e0b07407b3f4578cd37e01f75
vfdxvffd <2201986113@qq.com>
2021-02-04 22:59:08 +0800

    根据文件类型查看文件

---
+++ src/main/java/com/vfd/demo/controller/ClassifyFileController.java
@@ -0,0 +1,11 @@
+package com.vfd.demo.controller;
+
+/**
+ * @PackageName: com.vfd.demo.controller
+ * @ClassName: ClassifyFileController
+ * @Description:
+ * @author: vfdxvffd
+ * @date: 2021/2/4 下午6:06
+ */
+public class ClassifyFileController {
+}
