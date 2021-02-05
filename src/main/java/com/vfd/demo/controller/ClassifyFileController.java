eb3283c4ea60479d74de404386c789041d33d4b6
vfdxvffd <2201986113@qq.com>
2021-02-05 16:46:16 +0800

    增加根据文件类型查看文件的控制器

--- src/main/java/com/vfd/demo/controller/ClassifyFileController.java
+++ src/main/java/com/vfd/demo/controller/ClassifyFileController.java
@@ -1,5 +1,16 @@
 package com.vfd.demo.controller;
 
+import com.vfd.demo.bean.FileInfo;
+import com.vfd.demo.service.FileOperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.List;
+
 /**
  * @PackageName: com.vfd.demo.controller
  * @ClassName: ClassifyFileController
@@ -7,5 +18,45 @@ package com.vfd.demo.controller;
  * @author: vfdxvffd
  * @date: 2021/2/4 下午6:06
  */
+@Controller
 public class ClassifyFileController {
+
+    @Autowired
+    FileOperationService fileOperationService;
+
+    @ResponseBody
+    @RequestMapping("/find/document/{id}")
+    public List<FileInfo> findAllDocuments(@PathVariable("id") Integer id) {
+        return fileOperationService.getFilesByType(1,id);
+    }
+
+    @ResponseBody
+    @RequestMapping("/find/picture/{id}")
+    public List<FileInfo> findAllPictures(@PathVariable("id") Integer id) {
+        return fileOperationService.getFilesByType(2,id);
+    }
+
+    @ResponseBody
+    @RequestMapping("/find/video/{id}")
+    public List<FileInfo> findAllVideos(@PathVariable("id") Integer id) {
+        return fileOperationService.getFilesByType(3,id);
+    }
+
+    @ResponseBody
+    @RequestMapping("/find/audio/{id}")
+    public List<FileInfo> findAllAudios(@PathVariable("id") Integer id) {
+        return fileOperationService.getFilesByType(4,id);
+    }
+
+    @ResponseBody
+    @RequestMapping("/find/torrent/{id}")
+    public List<FileInfo> findAllTorrents(@PathVariable("id") Integer id) {
+        return fileOperationService.getFilesByType(5,id);
+    }
+
+    @ResponseBody
+    @RequestMapping("/find/others/{id}")
+    public List<FileInfo> findAllOthers(@PathVariable("id") Integer id) {
+        return fileOperationService.getFilesByType(6,id);
+    }
 }
