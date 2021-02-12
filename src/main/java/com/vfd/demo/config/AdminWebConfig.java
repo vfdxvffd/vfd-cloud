d38a6fce740f2770e5e2ddd3aa701ba8b770e5a1
vfdxvffd <2201986113@qq.com>
2021-02-13 00:19:21 +0800

    增加记住我功能，修改拦截器规则

--- src/main/java/com/vfd/demo/config/AdminWebConfig.java
+++ src/main/java/com/vfd/demo/config/AdminWebConfig.java
@@ -1,6 +1,7 @@
 package com.vfd.demo.config;
 
 import com.vfd.demo.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,13 +15,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class AdminWebConfig implements WebMvcConfigurer {
+    @Autowired
+    LoginInterceptor loginInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/","/login","/pages/register","/pages/forgot-password",
+                .excludePathPatterns("/pages/login","/login","/pages/register","/pages/forgot-password",
                         "/forget_password","/reset_password","/register","/sendCode","/preserve-file",
-                        "/pages/reset-password","/pages/share-file",
+                        "/pages/reset-password","/pages/share-file","/test","/shareLogin",
                         "/asserts/**","/webjars/**");
     }
 }
--- src/main/java/com/vfd/demo/interceptor/LoginInterceptor.java
+++ src/main/java/com/vfd/demo/interceptor/LoginInterceptor.java
@@ -1,9 +1,15 @@
 package com.vfd.demo.interceptor;
 
+import com.vfd.demo.bean.UserAccInfo;
+import com.vfd.demo.service.UserLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @PackageName: com.vfd.demo.interceptor
@@ -12,17 +18,52 @@ import javax.servlet.http.HttpServletResponse;
  * @author: vfdxvffd
  * @date: 2021/1/27 下午12:53
  */
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    UserLoginService userLoginService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Object userId = request.getSession().getAttribute("loginUserId");
-        if (userId == null) {
-            request.setAttribute("err","无权限访问，请先登陆");
-            request.getRequestDispatcher("/").forward(request,response);
-            //response.sendRedirect("/");
+        System.out.println("拦截了：" + request.getRequestURL());
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+//            request.setAttribute("err","无权限访问，请先登陆");
+            request.getRequestDispatcher("/pages/login").forward(request,response);
             return false;
-        } else {
-            return true;
         }
+        String userEmail = null;
+        Integer userPassword = null;
+        for (Cookie item:cookies) {
+            if ("loginEmail".equals(item.getName())) {
+                userEmail = item.getValue();
+            } else if ("loginPassword".equals(item.getName())) {
+                userPassword = Integer.parseInt(item.getValue());
+            }
+        }
+        if (userEmail == null || userPassword == null) {
+//            request.setAttribute("err","无权限访问，请先登陆");
+            System.out.println("为null");
+            request.getRequestDispatcher("/pages/login").forward(request,response);
+            return false;
+        }
+        HttpSession session = request.getSession();
+        Object loginUserId = session.getAttribute("loginUserId");
+        Object loginUserName = session.getAttribute("loginUserName");
+        System.out.println("id：" + loginUserId);
+        System.out.println("name：" + loginUserName);
+        if (loginUserId == null || loginUserName == null) {
+            UserAccInfo login = userLoginService.login(userEmail);
+            if (login == null) {
+                System.out.println("login == null");
+//                request.setAttribute("err","无权限访问，请先登陆");
+                request.getRequestDispatcher("/pages/login").forward(request,response);
+                return false;
+            }
+            session.setAttribute("loginUserId",login.getId());
+            session.setAttribute("loginUserName",login.getName());
+        }
+        return true;
     }
 }
