02a6121cfb7170de23a1b684cdf25b7e52cd9554
vfdxvffd <2201986113@qq.com>
2021-02-13 23:23:55 +0800

    消除程序中的魔法值

--- src/main/java/com/vfd/demo/utils/MagicValue.java
+++ src/main/java/com/vfd/demo/utils/MagicValue.java
@@ -3,9 +3,12 @@ package com.vfd.demo.utils;
 /**
  * @PackageName: com.vfd.demo.utils
  * @ClassName: MagicValue
- * @Description:
+ * @Description: 本来用来消除魔法值
  * @author: vfdxvffd
  * @date: 2021/2/13 下午9:13
  */
 public class MagicValue {
+
+    //拼接链接的前缀
+    public static String linkPrefix = "http://localhost:8080";
 }
