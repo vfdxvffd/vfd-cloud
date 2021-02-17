89330d79ad268d4db2de4189f4ce8c44ed660e5c
vfdxvffd <2201986113@qq.com>
2021-02-17 16:48:22 +0800

    增加table的右键功能

---
+++ src/main/java/com/vfd/demo/messaging/FileOperationConsumer.java
@@ -0,0 +1,11 @@
+package com.vfd.demo.messaging;
+
+/**
+ * @PackageName: com.vfd.demo.messaging
+ * @ClassName: FileOperationConsumer
+ * @Description:
+ * @author: vfdxvffd
+ * @date: 2021/2/16 下午2:58
+ */
+public class FileOperationConsumer {
+}
